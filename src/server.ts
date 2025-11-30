504ce25ac6b033bf18135ac0c498dfa1fa0d99b9
mdselimme <mdselimdev@gmail.com>
2025-12-01 00:44:19 +0600

    added: configure server and run the server

---
+++ src/server.ts
@@ -0,0 +1,53 @@
+import { Server } from "http";
+import app from "./app";
+import { envVars } from "./config/envVariable.config";
+
+let server: Server;
+
+// Bootstrap function to initialize the server
+const bootstrap = async () => {
+    try {
+        server = app.listen(envVars.PORT, () => {
+            console.log(`Server running on port http://localhost:${envVars.PORT}`)
+        })
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error("Error during server bootstrap:", error.message);
+        }
+    }
+};
+
+// Start the bootstrap process
+(async () => {
+    await bootstrap();
+})();
+
+const serverShutdown = async (message: string, err?: any) => {
+    console.log(`Message: ${message}. Server is closing.`, err || "")
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    };
+    process.exit(1);
+};
+
+//when server cloud platform stopped
+process.on("SIGTERM", () => {
+    serverShutdown("SIGTERM received");
+});
+
+//when server manually stopped
+process.on("SIGINT", () => {
+    serverShutdown("SIGINT received");
+});
+
+//when synchronous error occurs
+process.on("uncaughtException", (error) => {
+    serverShutdown("Uncaught Exception occurred", error);
+});
+
+//when promise rejection occurs
+process.on("unhandledRejection", (reason) => {
+    serverShutdown("Unhandled Rejection occurred", reason);
+});
