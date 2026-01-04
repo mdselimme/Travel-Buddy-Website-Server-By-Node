9635b110a9f54efac80dcebc814c78acee8e81f1
mdselimme <mdselimdev@gmail.com>
2026-01-04 23:40:18 +0600

    fix: rate limiter error message handle

--- src/app/middlewares/rateLimiter.ts
+++ src/app/middlewares/rateLimiter.ts
@@ -1,17 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import httpStatus from 'http-status-codes';
 import rateLimit from "express-rate-limit";
 import { envVars } from "../../config/envVariable.config";
+import ApiError from "../utils/ApiError";
 
 export const apiLimiter = rateLimit({
     windowMs: Number(envVars.RATE_LIMITER.RATE_LIMITER_WINDOW_MS), // 15 minutes
-    max: Number(envVars.RATE_LIMITER.RATE_LIMITER_API_MAX_REQUEST), // limit each IP to 100 requests per windowMs
-    message: "Too many requests from this IP, please try again after 15 minutes",
+    max: Number(envVars.RATE_LIMITER.RATE_LIMITER_API_MAX_REQUEST),
     standardHeaders: true,
     legacyHeaders: false,
+    handler: (req, res) => {
+        throw new ApiError(httpStatus.TOO_MANY_REQUESTS, "Too many requests from this IP, please try again later.");
+    },
 });
 
 export const authLimiter = rateLimit({
     windowMs: Number(envVars.RATE_LIMITER.RATE_LIMITER_WINDOW_MS),
     max: Number(envVars.RATE_LIMITER.RATE_LIMITER_AUTH_MAX_REQUEST),
-    message: "Too many login attempts, please try again later.",
+    handler: (req, res) => {
+        throw new ApiError(httpStatus.TOO_MANY_REQUESTS, "Too many login attempts from this IP, please try again later.");
+    },
     skipSuccessfulRequests: true,
 });
